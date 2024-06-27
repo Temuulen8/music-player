0dbfe14f1f770e12ea75719fe1d4f66266162bc6
Temuulen8 <b.temuulen2005@gmail.com>
2024-06-27 12:23:05 +0800

    m

--- music.js
+++ music.js
@@ -1,4 +1,15 @@
 const playBtn = document.getElementById("playBtn");
+const nextBtn = document.getElementById("nextBtn");
+const previous = document.getElementById("previousBtn");
+const audioPlayer = document.getElementsByTagName("audio")[0];
+const playList = [
+  { src: "./Clairo - Flamin Hot Cheetos.mp3" },
+  { src: "./Frank Ocean - White Ferrari.mp3" },
+  {
+    src: "./Puff Daddy - I'll Be Missing You (Lyrics) feat. Faith Evans, 112.mp3",
+  },
+];
+let currentSong = 0;
 
 playBtn.addEventListener("click", () => {
   //play song
@@ -22,3 +33,21 @@ playBtn.addEventListener("click", () => {
 // playBtn.addEventListener("click", change_word);
 // songs = ["song1 path", "songs2 path",  "songs3 path"]
 // next button -> songs[1], songs[2]
+nextBtn.addEventListener("click", () => {
+  currentSong++;
+  if (currentSong === playList.length) {
+    currentSong = 0;
+  }
+  audioPlayer.src = playList[currentSong].src;
+  audioPlayer.play();
+  console.log("CS", currentSong);
+});
+previous.addEventListener("click", () => {
+  currentSong--;
+  if (currentSong === playList.length) {
+    currentSong = 0;
+  }
+  audioPlayer.src = playList[currentSong].src;
+  audioPlayer.play();
+  console.log("CS", currentSong);
+});
